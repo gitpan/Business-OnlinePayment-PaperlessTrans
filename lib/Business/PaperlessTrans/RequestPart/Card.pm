54234aedd91d7aecadc6951b8abbb9e9673431f5
Caleb Cushing <xenoterracide@gmail.com>
2013-05-24 23:21:59 +0000

    Import of XENO/Business-OnlinePayment-PaperlessTrans-0.001001 from CPAN.
    gitpan-cpan-distribution: Business-OnlinePayment-PaperlessTrans
    gitpan-cpan-version:      0.001001
    gitpan-cpan-path:         XENO/Business-OnlinePayment-PaperlessTrans-0.001001.tar.gz
    gitpan-cpan-author:       XENO
    gitpan-cpan-maturity:     released

--- lib/Business/PaperlessTrans/RequestPart/Card.pm
+++ lib/Business/PaperlessTrans/RequestPart/Card.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use namespace::autoclean;
 
-our $VERSION = '0.001000'; # VERSION
+our $VERSION = '0.001001'; # VERSION
 
 use Moose;
 
@@ -17,7 +17,7 @@ with qw(
 );
 
 use MooseX::Types::Common::String qw( NonEmptySimpleStr );
-use MooseX::Types::CreditCard qw(
+use MooseX::Types::CreditCard 0.002 qw(
 	CardNumber
 	CardExpiration
 	CardSecurityCode
@@ -87,7 +87,7 @@ Business::PaperlessTrans::RequestPart::Card - Card
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 AUTHOR
 
